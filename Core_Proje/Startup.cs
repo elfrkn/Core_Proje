2b8cf1ff8391342211f30b82dade905f5c4fc55f
Elif ÇALIŞ <pcengelf@hotmail.com>
2024-03-11 13:16:14 +0100

    Authorize,Role,401 pages

--- Core_Proje/Startup.cs
+++ Core_Proje/Startup.cs
@@ -1,8 +1,11 @@
 using DataAccessLayer.Concrete;
 using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -28,6 +31,31 @@ namespace Core_Proje
             services.AddDbContext<Context>();
             services.AddIdentity<WriterUser, WriterRole>().AddEntityFrameworkStores<Context>();
             services.AddControllersWithViews();
+
+            services.AddMvc(Config =>
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                               .RequireAuthenticatedUser()
+                               .Build();
+                Config.Filters.Add(new AuthorizeFilter(policy));
+            });
+
+            services.AddMvc();
+            //services.AddAuthentication(
+            //    CookieAuthenticationDefaults.AuthenticationScheme)
+            //    .AddCookie(x =>
+            //    {
+            //        x.LoginPath = "/AdminLogin/Index/";
+            //    });
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.Cookie.HttpOnly = true;
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(10);
+                options.AccessDeniedPath = "/ErrorPage/Index/";
+                options.LoginPath = "/Writer/Login/Index/";
+               
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
