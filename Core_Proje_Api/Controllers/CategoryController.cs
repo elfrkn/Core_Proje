64c6721d38b9b05300d102f93966fcf96758364a
Elif ÇALIŞ <pcengelf@hotmail.com>
2024-03-13 08:30:40 +0100

    Api

---
+++ Core_Proje_Api/Controllers/CategoryController.cs
@@ -0,0 +1,81 @@
+﻿using Core_Proje_Api.DAL.ApiContext;
+using Core_Proje_Api.DAL.Entity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Core_Proje_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult CategoryList()
+        {
+           using var c = new Context();
+            return Ok(c.Categories.ToList());
+
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            using var c = new Context();
+            var values = c.Categories.Find(id);
+            if(values == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(values);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult CategoryAdd(Category p)
+        {
+            using var c = new Context();
+            c.Add(p);
+            c.SaveChanges();
+            return Created("",p);
+        }
+        [HttpDelete]
+
+        public IActionResult CategoryDelete(int id)
+        {
+            using var c = new Context();
+            var values = c.Categories.Find(id);
+            if(values == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                c.Remove(values);
+                c.SaveChanges();
+                return NoContent();
+            }
+        }
+
+        [HttpPut]
+        public IActionResult UpdateCategory(Category p)
+        {
+            using var c = new Context();
+            var value = c.Find<Category>(p.CategoryID);
+            if (value == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                value.CategoryName = p.CategoryName;
+                c.Update(value);
+                c.SaveChanges();
+                return NoContent();
+            }
+        }
+    }
+}
---
+++ Core_Proje_Api/DAL/ApiContext/Context.cs
@@ -0,0 +1,15 @@
+﻿using Core_Proje_Api.DAL.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core_Proje_Api.DAL.ApiContext
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=MSI;database=CoreProjeDb2;integrated security=true");
+        }
+
+        public  DbSet<Category> Categories { get; set; }
+    }
+}
